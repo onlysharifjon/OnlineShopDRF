898cb6158dfe975a4a434878407856f574d192b5
onlysharifjon <mominovsharif12@gmail.com>
2023-12-13 17:20:55 +0500

    First push

---
+++ ClientApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ClientappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ClientApp'
---
+++ SalerApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SalerappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SalerApp'
