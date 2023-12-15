5fa499215a14a7c89d5db35a8bbd31e6cb4bd816
Komoliddin <fazliddinovkamoliddin581@gmail.com>
2023-12-15 17:11:23 +0500

    Kamoliddin Login APIs push part 1

--- SalerApp/models.py
+++ SalerApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class SalerRegister(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=32)
+    PS_serial_num = models.IntegerField(unique=True)
+    PS_seria = models.CharField(max_length=2)
+    phone = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.login
--- SalerApp/views.py
+++ SalerApp/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import ForLoginSerializer
+from drf_yasg.utils import swagger_auto_schema
+from .models import SalerRegister
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+
 
 # Create your views here.
+class Register(APIView):
+    serializer_class = ForLoginSerializer
+
+    @swagger_auto_schema(request_body=ForLoginSerializer)
+    def post(self, request):
+        serializer = ForLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            login = request.data.get('login')
+            password = request.data.get('password')
+            seriya = request.data.get('PS_seria')
+            seriya_num = request.data.get('PS_serial_num')
+            phone = request.data.get('phone')
+            crt = SalerRegister.objects.create(login=login, phone=phone, password=password,
+                                               PS_serial_num=seriya_num, PS_seria=seriya)
+            user = SalerRegister.objects.filter(PS_seria=seriya, PS_serial_num=seriya_num, password=password).first()
+            if user:
+                acsess_token = AccessToken.for_user(user)
+                refresh_token = RefreshToken(user)
+                # serializer.save()  # Save the serializer after creating the user
+                return Response({"Register Success": str(acsess_token),
+                                 })
+            else:
+                return Response({"error": "User not found with provided credentials"},
+                                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUzum(APIView):
+    serializer_class = ForLoginSerializer
+
+    @swagger_auto_schema(request_body=ForLoginSerializer)
+    def post(self, request):
+        login = request.data.get('login')
+        password = request.data.get('password')
+        user = SalerRegister.objects.filter(login=login, password=password).first()
+        return Response({"Login Success": user.id})
