e64f9ef344e031872d1f97e676d7830ce9d83c22
ravshanovichpm <bilolbakhtiyorov44@gmail.com>
2023-12-15 17:24:48 +0500

    Userapp edit part 1

--- UserApp/models.py
+++ UserApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class UserModel(models.Model):
+    login = models.CharField(unique=True)
+    tel_raqam = models.IntegerField(unique=True, max_length=13)
+    password = models.CharField(max_length=32)
+    ps_seriya = models.CharField(max_length=2)
+    ps_seriya_raqam = models.IntegerField(unique=True)
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
--- UserApp/views.py
+++ UserApp/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from django.contrib.auth import authenticate
+from .serializers import ForLoginSerializer
+from drf_yasg.utils import swagger_auto_schema
+import jwt
 
-# Create your views here.
+
+class LoginUser(APIView):
+    serializer_class = ForLoginSerializer
+
+    @swagger_auto_schema(request_body=ForLoginSerializer)
+    def post(self, request):
+        seriya = request.data.get('ps_seriya')
+        seriya_raqam = request.data.get('ps_seriya_raqam')
+        password = request.data.get('password')
+        user = UserModel.objects.filter(ps_seriya=seriya, ps_seriya_raqam=seriya_raqam, password=password).first()
+        return Response({"Login Success": user.id})
+
+
+class Register(APIView):
+    serializer_class = ForLoginSerializer
+
+    @swagger_auto_schema(request_body=ForLoginSerializer)
+    def post(self, request):
+        serializer = ForLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            seriya = request.data.get('ps_seriya')
+            seriya_raqam = request.data.get('ps_seriya_raqam')
+            password = request.data.get('password')
+            login = request.data.get('login')
+            tel_raqam = request.data.get('tel_raqam')
+            crt = UserModel.objects.create(tel_raqam=tel_raqam, login=login, password=password,
+                                           ps_seriya_raqam=seriya_raqam, ps_seriya=seriya)
+            user = UserModel.objects.filter(ps_seriya=seriya, ps_seriya_raqam=seriya_raqam, password=password).first()
+            if user:
+                access_token = AccessToken.for_user(user)
+                refresh_token = RefreshToken(user)
+                return Response({"Register Access": str(access_token),
+                                 })
+            else:
+                return Response({"error": "User not found with provided credentials"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
